ebbf4b1ff97263ff8f1361b6d224321e91c13a51
yogitashete2412 <yogitashete24@gmail.com>
2023-05-22 17:12:43 +0530

    doc - changes in documentation

---
+++ Math Function/index.js
@@ -0,0 +1,23 @@
+const a = 1.9;
+const b = 1.2;
+const c = 8.7;
+
+//IN the math function we want to see always after pointing value
+//ceil function value is 1.4 its op is 2
+console.log(Math.ceil(a));
+
+//floor function value is 2.3 its op is 2
+ console.log(Math.floor(b));
+
+//round function check after the pointing value is 0.5>= it works like a ceil() and value is 0.5<= it works like a floor()
+console.log(Math.round(c));
+
+//abs is always return +ve value
+const x = -7;
+console.log(Math.abs(x))
+
+//pow - 1st is the base and 2nd is the power 
+console.log(Math.pow(3,2))
+
+//sqrt - 
+console.log(Math.sqrt(64))
